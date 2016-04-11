380da7286733c0dd316e1d8b7b2cadde463f4a8d
sutaking <zhaofeng.vip@gmail.com>
2016-04-11 15:24:21 +0800

    add new file

--- sample/app.jsx
+++ sample/app.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ListDemo from './listSample.jsx';
+import ListDemo from './listSample';
 
 require('../style/base.css')
 
-const appComponent = (< div >
-    <div className={'demo-title'}>React CAPH Showcase</div>
+const appComponent = (<div>
+    <div className={'demo-title'}>React ListDemo</div>
     <ListDemo/>
     </div>);
 
